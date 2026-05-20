bac20090e9bf0ce8464cb0bf5b0806160682b9f2
Gamermees <mejhuizingh@gmail.com>
2026-05-20 13:10:44 +0200

    DASHH

--- serious-noob-jam/Scripts/catbox.gd
+++ serious-noob-jam/Scripts/catbox.gd
@@ -36,3 +36,26 @@ func update_animation():
 
 func update_gravity(delta):
 	velocity.y += gravity * delta
+
+#dash
+var hold_time = 0.0
+var hold_threshold = 3.0
+var dash_speed = 600.0
+
+func _process(delta):
+	if Input.is_action_pressed("dash"):
+		hold_time += delta
+		var progress = hold_time / hold_threshold
+		print("Hold progress: ", progress * 100, "%")
+		
+		if hold_time >= hold_threshold:
+			execute_dash()
+			hold_time = 0.0
+	else:
+		hold_time = 0.0
+
+func execute_dash():
+	var input_vector = Input.get_vector("R_dash", "L_dash", "up_dash", "Down_dash")
+	velocity = input_vector.normalized() * dash_speed
+	move_and_slide()  # Actually move the character!
+	print("dash")
